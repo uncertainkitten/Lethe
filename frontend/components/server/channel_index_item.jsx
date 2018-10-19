e9ea5f04f6403b67e1000328fe1f612821185445
Cassandra McClure <uncertainkitten@gmail.com>
2018-10-19 12:12:01 -0700

    Create channel modal selection

--- frontend/components/server/channel_index_item.jsx
+++ frontend/components/server/channel_index_item.jsx
@@ -1,28 +1,43 @@
 import React from 'react';
+import {withRouter} from 'react-router-dom';
 
 class ChannelIndexItem extends React.Component {
   constructor(props){
     super(props);
     this.state = {
-      selected: "channelItem",
-      channel: this.props.channel
+      channel: this.props.channel,
+      mode: this.props.mode
     }
     this.handleClick = this.handleClick.bind(this);
   }
 
   handleClick(e) {
     e.preventDefault();
-    this.setState({selected: "selectedChannelItem"});
-    this.props.history.push(`/servers/${this.state.channel.server_id}/channels/${this.state.channel.id}`);
+    e.stopPropagation();
+    if (this.state.mode !== `C${this.state.channel.id}`) {
+      this.props.fetchChannel(this.state.channel.id)
+      this.props.openModal(`C${this.state.channel.id}`)
+      this.setState({mode: this.props.mode});
+      this.props.history.push(`/servers/${this.state.channel.server_id}/channels/${this.state.channel.id}`);
+    } else {
+      this.setState({mode: this.props.mode});
+      this.props.closeModal();
+    }
   }
 
   render() {
+    let stateClass;
+    if (this.props.mode === `C${this.state.channel.id}`) {
+      stateClass = "selectedChannelItem";
+    } else {
+      stateClass = "channelItem";
+    }
     return (
       <div className="channelItemContainer">
-        <span className={this.state.selected} onClick={this.handleClick}># {this.props.channel.name}</span>
+        <span className={stateClass} onClick={this.handleClick}># {this.props.channel.name}</span>
       </div>
     );
   }
 }
 
-export default ChannelIndexItem;
+export default withRouter(ChannelIndexItem);
