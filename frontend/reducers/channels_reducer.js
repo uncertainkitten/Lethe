613091aaaa87a231cdf31736f4b269091ecc51c4
Cassandra McClure <uncertainkitten@gmail.com>
2018-10-19 06:13:13 -0700

    Create channels reducer

---
+++ frontend/reducers/channels_reducer.js
@@ -0,0 +1,22 @@
+import { RECEIVE_CHANNEL, RECEIVE_CHANNELS, REMOVE_CHANNEL } from '../actions/channel_actions';
+import { merge } from 'lodash';
+
+const channelReducer = (state = {}, action) => {
+  Object.freeze(state);
+  switch (action.type) {
+    case RECEIVE_CHANNELS:
+      return merge({}, state, action.channels);
+    case RECEIVE_CHANNEL:
+      return merge({}, state, { [action.channel.id]: action.channel });
+    case REMOVE_CHANNEL:
+      let newState = {};
+      Object.assign(newState, state);
+      delete newState[action.channel.id];
+      return newState;
+    default:
+      return state;
+  }
+}
+
+
+export default channelReducer;
--- frontend/reducers/entities_reducer.js
+++ frontend/reducers/entities_reducer.js
@@ -3,12 +3,14 @@ import UsersReducer from './users_reducer';
 import ServersReducer from './servers_reducer';
 import MembershipsReducer from './memberships_reducer';
 import InvitesReducer from './invites_reducer';
+import ChannelsReducer from './channels_reducer';
 
 const entitiesReducer = combineReducers({
   users: UsersReducer,
   servers: ServersReducer,
   members: MembershipsReducer,
-  invites: InvitesReducer
+  invites: InvitesReducer,
+  channels: ChannelsReducer
 });
 
 export default entitiesReducer;
