98b13e3c77c5ca270b39cb656b6f4baaa7ea1210
Cassandra <uncertainkitten@gmail.com>
2018-11-21 00:06:29 -0800

    Continue ActionCable integration

--- frontend/components/server/message_index.jsx
+++ frontend/components/server/message_index.jsx
@@ -1,8 +1,5 @@
 import React from 'react';
-import NewMessageForm from './new_message_form';
-import Cable from './cables';
-import { ActionCable } from 'react-actioncable-provider';
-import { API_ROOT } from '../../util/constants';
+import NewMessageFormContainer from './new_message_form_container';
 import {withRouter} from 'react-router-dom';
 
 class MessageIndex extends React.Component{
@@ -11,50 +8,11 @@ class MessageIndex extends React.Component{
     this.props.fetchMessages()
   }
 
-  componentWillReceiveProps(nextProps, nextState) {
-    if (this.state.channel !== nextState.channel){
-      this.setState({messages: nextState.channel.messages});
-    }
-  }
-
-  handleReceivedChannel() {
-    this.props.fetchChannel(this.props.channelId)
-    .then(res => this.setState({channel: res.channel}));
-  }
-
-  handleReceivedMessage() {
-    if (this.state.channel){
-      this.setState({ messages: this.state.channel.messages });
-    }
-  }
-
-  orderedMessages() {
-    if (this.state.messages) {
-      const sortedMessages = this.state.messages.sort((a, b) => new Date(a.created_at) - new Date(b.created_at));
-      return sortedMessages.map(message => {<li key={message.id}>{message.body}</li>});
-    } else {
-      return "No messages here!";
-    }
-  }
-
-
   render() {
     return (
       <div className="messageIndex">
-      <ActionCable
-        channel={{channel: "ChannelsChannel"}}
-        onReceived={this.handleReceivedChannel}
-      />
-      {this.state.channel ? (
-        <Cable
-          channel={this.state.channel}
-          handleReceivedMessage={this.handleReceivedMessage}
-        />
-      ) : null}
-      )
-      <div className="messages">{this.orderedMessages()}</div>
         <div className="messageInputWrapper">
-          <NewMessageForm className="messageInput" channelId={this.state.channelId} userId={this.props.currentUser.id}> </NewMessageForm>
+          <NewMessageFormContainer className="messageInput" />
         </div>
       </div>
     );
